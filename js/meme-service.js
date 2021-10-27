d48339d6e2540f2cd6ed41f675f2453cbd8f4151
Paz Balter <pazbalterpb@gmail.com>
2021-10-27 16:04:51 +0300

    adding input

--- js/meme-service.js
+++ js/meme-service.js
@@ -1,11 +1,60 @@
 'use strict'
 
-
+const topTextInput = document.querySelector('.topTextInput')
+const bottomTextInput = document.querySelector('.bottomTextInput')
+const elCanvas = document.querySelector('#meme-canvas')
 
 function getImgSrc(){
     var img = gImgs[gMeme.selectedImgId]
     return img.url;
 }
+
+
+topTextInput.addEventListener("change", () =>{
+    updateMemeCanvas(elCanvas, topTextInput.value,bottomTextInput.value) // image value
+});
+bottomTextInput.addEventListener("change", () =>{
+    updateMemeCanvas(elCanvas, topTextInput.value,bottomTextInput.value) // image value
+});
+
+
+
+
+
+function updateMemeCanvas(elCanvas,topText,bottomText){  // image value
+    
+    var ctx = elCanvas.getContext("2d");
+    var width = elCanvas.width
+    var height = elCanvas.height
+    var fontSize = Math.floor(width/10);
+    var yOffset = height/25;
+    console.log(fontSize)
+
+    // update canvas img
+    // insertImgToCanvas()
+
+    // prepare text
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = Math.floor(fontSize/4);
+    ctx.fillStyle = 'white';
+    ctx.textAlign = 'center';
+    ctx.lineJoin = "round";
+    ctx.font = `${fontSize}px meme`
+
+    // add top text
+
+    ctx.textBaseline = 'top'
+    ctx.strokeText(topText,width/2,yOffset)
+    ctx.fillText(topText,width/2,yOffset)
+
+    // add bottom text
+
+    ctx.textBaseline = 'bottom'
+    ctx.strokeText(bottomText,width/2,height - yOffset)
+    ctx.fillText(bottomText,width/2,height - yOffset)
+}
+
+
 
 function insertImgToCanvas(){
     var img = new Image()
@@ -15,7 +64,13 @@ function insertImgToCanvas(){
     img.onload = () => {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height) 
     }
-    // ctx.drawImage(img,0,0,canvas.width,canvas.height)
-
-
 }
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = 'black';
+    gCtx.fillStyle = 'white';
+    gCtx.font = '30px  meme';
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+  }
