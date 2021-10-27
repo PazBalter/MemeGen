196b19bf7b6b99d99a4a9736a18db0895d3c57b6
Paz Balter <pazbalterpb@gmail.com>
2021-10-27 19:18:56 +0300

    lowercase

--- js/meme-service.js
+++ js/meme-service.js
@@ -7,24 +7,24 @@ var gKeywords = {'happy':0,
 }
 
 var gImgs = [
-    {id: 1, url: 'img-squareMeme/1.jpg', keywords: ['happy']},
-    {id: 2, url: 'img-squareMeme/2.jpg', keywords: ['happy']},
-    {id: 3, url: 'img-squareMeme/3.jpg', keywords: ['happy']},
-    {id: 4, url: 'img-squareMeme/4.jpg', keywords: ['happy']},
-    {id: 5, url: 'img-squareMeme/5.jpg', keywords: ['happy']},
-    {id: 6, url: 'img-squareMeme/6.jpg', keywords: ['happy']},
-    {id: 7, url: 'img-squareMeme/7.jpg', keywords: ['happy']},
-    {id: 8, url: 'img-squareMeme/8.jpg', keywords: ['happy']},
-    {id: 9, url: 'img-squareMeme/9.jpg', keywords: ['happy']},
-    {id: 10, url: 'img-squareMeme/10.jpg', keywords: ['happy']},
-    {id: 11, url: 'img-squareMeme/11.jpg', keywords: ['happy']},
-    {id: 12, url: 'img-squareMeme/12.jpg', keywords: ['happy']},
-    {id: 13, url: 'img-squareMeme/13.jpg', keywords: ['happy']},
-    {id: 14, url: 'img-squareMeme/14.jpg', keywords: ['happy']},
-    {id: 15, url: 'img-squareMeme/15.jpg', keywords: ['happy']},
-    {id: 16, url: 'img-squareMeme/16.jpg', keywords: ['happy']},
-    {id: 17, url: 'img-squareMeme/17.jpg', keywords: ['happy']},
-    {id: 18, url: 'img-squareMeme/18.jpg', keywords: ['happy']}
+    {id: 1, url: 'img-square-meme/1.jpg', keywords: ['happy']},
+    {id: 2, url: 'img-square-meme/2.jpg', keywords: ['happy']},
+    {id: 3, url: 'img-square-meme/3.jpg', keywords: ['happy']},
+    {id: 4, url: 'img-square-meme/4.jpg', keywords: ['happy']},
+    {id: 5, url: 'img-square-meme/5.jpg', keywords: ['happy']},
+    {id: 6, url: 'img-square-meme/6.jpg', keywords: ['happy']},
+    {id: 7, url: 'img-square-meme/7.jpg', keywords: ['happy']},
+    {id: 8, url: 'img-square-meme/8.jpg', keywords: ['happy']},
+    {id: 9, url: 'img-square-meme/9.jpg', keywords: ['happy']},
+    {id: 10, url: 'img-square-meme/10.jpg', keywords: ['happy']},
+    {id: 11, url: 'img-square-meme/11.jpg', keywords: ['happy']},
+    {id: 12, url: 'img-square-meme/12.jpg', keywords: ['happy']},
+    {id: 13, url: 'img-square-meme/13.jpg', keywords: ['happy']},
+    {id: 14, url: 'img-square-meme/14.jpg', keywords: ['happy']},
+    {id: 15, url: 'img-square-meme/15.jpg', keywords: ['happy']},
+    {id: 16, url: 'img-square-meme/16.jpg', keywords: ['happy']},
+    {id: 17, url: 'img-square-meme/17.jpg', keywords: ['happy']},
+    {id: 18, url: 'img-square-meme/18.jpg', keywords: ['happy']}
 ];
 
 var gMeme = {
